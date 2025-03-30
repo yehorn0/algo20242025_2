bb21661e267ecf481dd7e43e4c7b5eedb09589e9
yehorn0 <nemch.egor.fl@gmail.com>
2025-03-30 10:26:23 +0300

    Midterm template

---
+++ midterm/mid_2.py
@@ -0,0 +1,18 @@
+def process_scores(scores: list[tuple[str, int]]) -> dict[str, int]:
+    ...
+
+
+scores = [
+    ('Alice', 85),
+    ('Bob', 75),
+    ('Charlie', 39),
+    ('David', 62),
+    ('Eve', 91)
+]
+"""
+{
+    "Eve": 91,
+    "Alice": 85,
+    "Bob": 75,
+}
+"""
